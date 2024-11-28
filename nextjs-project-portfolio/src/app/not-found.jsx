63b08ed9f86abb5b30c0e1a96133e9e0cf35dac5
Juaigles <kiookaix@gmail.com>
2024-11-29 00:12:23 +0100

    not-found y bug-api-post-id

--- nextjs-project-portfolio/src/app/not-found.jsx
+++ nextjs-project-portfolio/src/app/not-found.jsx
@@ -1,9 +1,14 @@
+import styles from "./page.module.css"
+
 export default function NotFound() {
     return (
-      <div>
+        <div className={styles.notfound}>
+
+      <div className="">
         <h1>404 - Página no encontrada</h1>
-        <p>Lo sentimos, no pudimos encontrar esta página.</p>
+        <p>Lo sentimos, no hemos podido encontrar esta página.</p>
       </div>
+        </div>
     );
   }
   
