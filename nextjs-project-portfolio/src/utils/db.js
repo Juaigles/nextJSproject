bfbe64fe4e7bfee5f7a9c39742bd4d5e452dc824
Juaigles <kiookaix@gmail.com>
2024-11-28 18:13:49 +0100

    MONGO-to-MONGODB_URI-env-variable

--- nextjs-project-portfolio/src/utils/db.js
+++ nextjs-project-portfolio/src/utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
     mongoose.set('strictQuery', false);
     try {
-        await mongoose.connect(process.env.MONGO)
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("DB is connected");
     } catch (error) {
         throw new Error('Connection failed!')
