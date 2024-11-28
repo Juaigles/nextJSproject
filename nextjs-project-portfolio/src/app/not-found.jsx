1cae07ca96f729ec5ddfeb87050f02ebb200ccf9
Juaigles <kiookaix@gmail.com>
2024-11-28 12:40:31 +0100

    not-found-solved

---
+++ nextjs-project-portfolio/src/app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <div>
+        <h1>404 - Página no encontrada</h1>
+        <p>Lo sentimos, no pudimos encontrar esta página.</p>
+      </div>
+    );
+  }
+  
